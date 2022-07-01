1351d331f96eb75c29c1eba81a6a290f4d3ed9fe
KrishnaM03 <96728162+KrishnaM03@users.noreply.github.com>
2022-07-01 11:44:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+NoseX = 0;
+NoseY = 0;
+
+function preload() {
+    moustache = loadImage("https://i.postimg.cc/rwfbTfjP/istockphoto-485318064-612x612-removebg-preview.png")
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+    image(moustache, NoseX, NoseY, 50, 50);
+}
+
+function take_snapshot() {
+    save('myFilterImage.png')
+}
+
+function modelLoaded() {
+    console.log('PoseNet is initialised');
+}
+
+function gotPoses(results) {
+    if(results.length > 0) 
+    {
+        console.log(results);
+        NoseX = results[0].pose.nose.x - 20;
+        NoseY = results[0].pose.nose.y - 5;
+        console.log("nose x = " + NoseX);
+        console.log("nose y = " + NoseY);
+    }
+}
